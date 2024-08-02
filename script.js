09b2480f7472e26c1e1068c89aa154dba6d496e3
bernadettadhealova <135321874+bernadettadhealova@users.noreply.github.com>
2024-08-03 05:20:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('openButton').addEventListener('click', function() {
+    document.getElementById('content').classList.remove('hidden');
+    this.style.display = 'none';
+});
+
